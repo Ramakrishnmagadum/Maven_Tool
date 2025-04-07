eb4a655320da0378750316afb39ed609d370174b
Ramakrishna Magadum Kallappa <ramakrishnamagadum.k@auracloud.com>
2025-04-07 10:47:23 +0530

    Maven Build Tool Analyizng how it works

---
+++ Maven_Project/java/com/sample/Maven_Project/sample.java
@@ -0,0 +1,7 @@
+package com.sample.Maven_Project;
+
+public class sample {
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
---
+++ Maven_Project/src.nonstandard.java/sample/Application1.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class Application1 {
+public static void main(String[] args) {
+	System.out.println("hello");
+}
+}
